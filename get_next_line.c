7f0e293759e90bf584861719636736d8bc2905c4
Florian Hensel <florinator543@gmail.com>
2025-11-06 16:36:23 +0100

    fixed some edgecases

--- get_next_line.c
+++ get_next_line.c
@@ -1,62 +1,62 @@
 #include "get_next_line.h"
 
-static char *free_and_return(char *buffer, char *stash)
+static char	*free_and_return(char *buffer, char *stash)
 {
-    free(buffer);
-    free(stash);
-    return (NULL);
+	free(buffer);
+	free(stash);
+	return (NULL);
 }
 
-static char *handle_newline(char **stash_ptr, char *nl)
+static char	*handle_newline(char **stash_ptr, char *nl)
 {
-    char *line;
-    char *tmp;
-    
-    line = ft_substr(*stash_ptr, 0, nl - *stash_ptr + 1);
-    tmp = ft_strdup(nl + 1);
-    free(*stash_ptr);
-    *stash_ptr = tmp;
-    if (!line || !tmp)
-    {
-        free(line);
-        free(*stash_ptr);
-        *stash_ptr = NULL;
-        return (NULL);
-    }
-    return (line);
+	char	*line;
+	char	*tmp;
+
+	line = ft_substr(*stash_ptr, 0, nl - *stash_ptr + 1);
+	tmp = ft_strdup(nl + 1);
+	free(*stash_ptr);
+	*stash_ptr = tmp;
+	if (!line || !tmp)
+	{
+		free(line);
+		free(*stash_ptr);
+		*stash_ptr = NULL;
+		return (NULL);
+	}
+	if (tmp[0] == '\0')
+	{
+		free(*stash_ptr);
+		*stash_ptr = NULL;
+	}
+	return (line);
 }
 
-static char *extract_line(char **stash_ptr)
+static char	*extract_line(char **stash_ptr)
 {
-    char *nl;
-    char *line;
-    
-    if (!*stash_ptr)
-        return (NULL);
-    nl = ft_strchr(*stash_ptr, '\n');
-    if (nl)
-        return (handle_newline(stash_ptr, nl));
-    line = ft_strdup(*stash_ptr);
-    free(*stash_ptr);
-    *stash_ptr = NULL;
-    return (line);
-}
+	char	*nl;
+	char	*line;
 
-static char *join_buffer(char *stash, char *buffer)
-{
-    char *tmp;
-    
-    tmp = ft_strjoin(stash, buffer);
-    if (!tmp)
-        return (NULL);
-    free(stash);
-    return (tmp);
+	if (!*stash_ptr || (*stash_ptr)[0] == '\0')
+	{
+		if (*stash_ptr)
+			free(*stash_ptr);
+		*stash_ptr = NULL;
+		return (NULL);
+	}
+	nl = ft_strchr(*stash_ptr, '\n');
+	if (nl)
+		return (handle_newline(stash_ptr, nl));
+	line = ft_strdup(*stash_ptr);
+	free(*stash_ptr);
+	*stash_ptr = NULL;
+	return (line);
 }
 
 static char	*read_line(int fd, char *stash)
 {
 	int		count;
 	char	*buffer;
+	char	*tmp;
 
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
@@ -70,24 +70,24 @@ static char	*read_line(int fd, char *stash)
 		if (count == 0)
 			break ;
 		buffer[count] = '\0';
-		stash = join_buffer(stash, buffer);
+		tmp = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = tmp;
 		if (!stash)
-			return (free_and_return(buffer, NULL));
+			return (free_and_return(buffer, stash));
 	}
 	free(buffer);
 	return (stash);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *stash;
-    char *line;
-    
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    stash = read_line(fd, stash);
-    if (!stash)
-        return (NULL);
-    line = extract_line(&stash);
-    return (line);
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_line(fd, stash);
+	line = extract_line(&stash);
+	return (line);
 }
